7bcd583ecea27b538817b4a2fd600092b703b684
Nelson Christovam Neto <netoncn@hotmail.com>
2019-11-13 10:57:20 -0300

    Aula 24

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,17 @@
-module.exports = function(app){
-    app.get('/noticias', function(req,res){
-        res.render('noticias/noticias');
-    });
+module.exports = function (app) {
+  app.get('/noticias', function (req, res) {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: '192.185.223.161',
+      user: 'heynerdc_pn',
+      password: '123456',
+      database: 'heynerdc_pn'
+    })
+
+    connection.query('SELECT * FROM noticias', function (error, result) {
+      res.send(error || result)
+    })
+
+    // res.render('noticias/noticias')
+  })
 }
